d7d2a22e869fc711e5ad5e20a6787aef4cf3119a
anjutshaju <anjutshaju@mca.ajce.in>
2020-10-19 19:34:04 +0530

    Updates

---
+++ precache-manifest.548ca87705cdc8e03d3d7995f4d9f63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8efc831eb41c40458c2d3456a6cfb4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c084cc565f7ad2420091",
+    "url": "/monsters-rolodex/static/css/main.4adbb420.chunk.css"
+  },
+  {
+    "revision": "56aae774d8f9eeb2cd6c",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c084cc565f7ad2420091",
+    "url": "/monsters-rolodex/static/js/main.c66bbf13.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
